1922cc2a00a00a10fc78692a955c0edfae10e77e
OlekKovalevych <kovalevich.148@gmail.com>
2022-05-18 12:29:34 +0200

    delete button for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
 
   # DELETE /users/1 or /users/1.json
   def destroy
-    @articles.destroy
+    @articles.delete_all
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
