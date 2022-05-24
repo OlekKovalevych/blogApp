f9967ae447d63e156eee32af0e848af8fb2e4b2e
OlekKovalevych <kovalevich.148@gmail.com>
2022-05-24 09:16:33 +0200

    UserController all test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,17 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
-  let(:valid_user_params) do
-    { user: { name: 'Name', surname: 'Surname' } }
-  end
-  let(:invalid_user_params) do
-    { user: { name: nil, surname: 'Surname' } }
-  end
-  let(:update_user_params) do
+  let(:user) { create :user }
+  let(:valid_update_user_params) do
     { id: user.id, user: { name: 'Name', surname: 'Surname' } }
   end
-
   describe '[GET] #index' do
     context 'with successful response' do
       before { get :index }
@@ -40,7 +33,12 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe '[POST] #create' do
-
+    let(:valid_user_params) do
+      { user: { name: 'Name', surname: 'Surname' } }
+    end
+    let(:invalid_user_params) do
+      { user: { name: nil, surname: 'Surname' } }
+    end
     context 'with successful response' do
       before { post :create, params: valid_user_params }
 
@@ -55,11 +53,23 @@ RSpec.describe UsersController, type: :controller do
 
   describe '[PATH] #update' do
     context 'with successful response' do
-      let(:user) { create :user }
-      before { put :update, params: update_user_params }
+      before { put :update, params: valid_update_user_params }
       it { expect(response).to redirect_to user_path(id: user.id) }
+    end
+
+    context 'with error' do
+      let(:invalid_update_user_params) do
+        { id: user.id, user: { name: nil, surname: 'surname' } }
+      end
+      before { put :update, params: invalid_update_user_params }
+      it { expect(response).to render_template(:edit) }
     end
   end
 
-  describe '[DELETE] destroy'
+  describe '[DELETE] destroy' do
+    context 'with successful response' do
+      before { delete :destroy, params: valid_update_user_params }
+      it { expect(response).to redirect_to users_path }
+    end
+  end
 end
