92bc4b415a1a92686c613f12a5cdc75f89241b7c
OlekKovalevych <kovalevich.148@gmail.com>
2022-06-01 12:14:38 +0200

    validation for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # i tutaj tę linijkę bym wyrzucił do poszczególnych kontrolerów
   before_action :authenticate_user!, only: %i[create new destroy]
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sing_up, keys: %i[name surname is_admin])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_validation :set_is_admin, on: %i[create save]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
 
-  validates :is_admin, presence: true
   validates :name, presence: true
   validates :surname, presence: true
 
   def admin?
     is_admin
   end
+end
+
+private
+
+def set_is_admin
+  @is_admin ||= false
 end
